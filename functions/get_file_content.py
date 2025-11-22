d0349b6092ed3d51d44e2a64871435cbf0f92c49
Niruel <npruppel@gmail.com>
2025-11-22 00:55:34 -0600

    D: added two function to get and write files.

---
+++ functions/get_file_content.py
@@ -0,0 +1,24 @@
+import os
+
+#function defenition
+def get_file_content(working_directory, file_path):
+    
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory,file_path))
+    print(abs_file_path)
+    if(not abs_file_path.startswith(abs_working_dir)):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if(not os.path.isfile(abs_file_path)):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        MAX_CHARS = 10000
+
+        with open(abs_file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path)> MAX_CHARS:
+                file_content_string += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return file_content_string
+    except Exception as e:
+         return f"Error read file with code: {e}"
